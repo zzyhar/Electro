d2b8eb4ae5db8e7eb9ad2ba9e285b03976f59c07
zzyhar <93952085+zzyhar@users.noreply.github.com>
2022-06-30 13:10:50 -0400

    Scroll up JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,6 +82,14 @@ linkFeatured.forEach((l) => l.addEventListener("click", activeFeatured));
 
 /*=============== SHOW SCROLL UP ===============*/
 
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
